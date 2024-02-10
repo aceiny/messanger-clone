dbb800e246f60cd0e4ec1d6d3273fdeeb0d29745
aceiny <a_zeraibi@estin.dz>
2024-02-10 04:16:06 +0100

    fixed

--- app/(home)/_componants/chat/ChatBody.tsx
+++ app/(home)/_componants/chat/ChatBody.tsx
@@ -3,11 +3,16 @@ import ChatMessage from './ChatMessage'
 
 const ChatBody = () => {
   return (
-    <div className='flex-1 flex flex-col-reverse px-3'>
+    <div className='flex-1 overflow-y-auto gap-1 flex flex-col-reverse px-3'>
         <ChatMessage
         sender="you"
         context='outgoing'
         sent={true}
+        />
+        <ChatMessage
+        sender="other"
+        context='ingoing'
+        sent={false}
         />
     </div>
   )
--- app/(home)/_componants/chat/ChatMessage.tsx
+++ app/(home)/_componants/chat/ChatMessage.tsx
@@ -13,7 +13,7 @@ const ChatMessage = ({sender , context , sent} : Props) => {
         {
             sent ? null : <img src="https://img.freepik.com/photos-gratuite/prise-vue-au-grand-angle-seul-arbre-poussant-sous-ciel-assombri-pendant-coucher-soleil-entoure-herbe_181624-22807.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1707436800&semt=sph" alt="image" className='w-[30px] object-cover  h-[30px] rounded-full ' />
         }
-        <div className={cn(' py-2 px-3 rounded-full' , sent?"bg-[#0A7CFF] text-white":"bg-[#f0f0f0] text-[#050505]")}>this is a message</div>
+        <div className={cn(' py-2 px-3 rounded-full' , sent?"bg-[#0A7CFF] text-white":"bg-[#f0f0f0] text-[#050505]")}>{context}</div>
     </div>
   )
 }
